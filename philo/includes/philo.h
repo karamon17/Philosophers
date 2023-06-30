a8b6bc54e61a4520fd9b6b2cc6719e55e7e00eed
karamon17 <gevorik18@gmail.com>
2023-06-30 14:23:52 +0400

    check arguments, create and init t_data structure

---
+++ philo/includes/philo.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhaishb <gkhaishb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/30 12:27:18 by gkhaishb          #+#    #+#             */
+/*   Updated: 2023/06/30 14:08:25 by gkhaishb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <stdint.h>
+# include <pthread.h>
+
+typedef struct s_data
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	optional_arg;
+}	t_data;
+
+
+int	ft_atoi(const char *str);
+
+#endif
